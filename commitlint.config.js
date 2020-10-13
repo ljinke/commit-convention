1baeebc7cef746f6f6f8bf285794483ec23eed0a
Jackie Liu <jinke.liu@autodesk.com>
2020-10-13 14:18:04 +0800

    chore: extend convention

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // Place your rules here
+    "scope-enum": [2, "always", ["core", "api"]], // error if scope is given but not in provided list
+  },
+};
